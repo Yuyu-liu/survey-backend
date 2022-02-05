87ef9ab65234785f9701034d83eba5c38808aba8
Alice Liu <alice.liu@paradocs.ca>
2022-02-05 13:20:10 -0500

    initial commit

---
+++ src/main/java/com/surveybackend/Main.java
@@ -0,0 +1,21 @@
+package com.surveybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
+
+@Controller
+@SpringBootApplication
+public class Main {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+}
